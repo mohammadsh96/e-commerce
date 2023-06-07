58643dbe903f3e63923cd4b86a18da41f542b8c4
KZTahat <kztahat96@gmail.com>
2023-06-07 11:12:13 +0000

    set up redux files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      welcome Khaled
+      Thank you Mohammed
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { productStore } from './redux';
+import { Provider } from 'react-redux';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={productStore}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
-
